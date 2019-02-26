b6fe9c2aa684acadf331b1aeb5635daf29741e68
wessberg <frederikwessberg@gmail.com>
2019-02-26 22:02:38 +0100

    Renamed a file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {SUPPORTS_LIST_FORMAT} from "./support/supports-relative-time-format";
+import {SUPPORTS_LIST_FORMAT} from "./support/supports-intl-list-format";
 import {patch} from "./patch/patch";
 
 if (!SUPPORTS_LIST_FORMAT) {
